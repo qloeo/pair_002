88e236bff33a38bf16eedc4e595e9027c5c4c48b
ruby04 <ruby04@SHUREE-MacBook-Air.local>
2023-04-13 20:52:20 +0900

    merge_conflicts

--- reviews/models.py
+++ reviews/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 class Review(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=80)
-    content= models.CharField(max_length=500)
+    content= models.TextField()
     movie = models.CharField(max_length=80)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- reviews/views.py
+++ reviews/views.py
@@ -7,9 +7,9 @@ from accounts.forms import CustomUserCreationForm, CustomUserChangeForm
 # Create your views here.
 
 def index(request):
-    
+    reviews = Review.objects.all()
     context = {
-        
+        'reviews': reviews,
     }
     return render(request, 'reviews/index.html', context)
 
