e546c3430760dab672c370ae76bd0859523d811c
qloeo <https://github.com/qloeo>
2023-04-13 14:21:42 +0900

    createmodels

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Review, Comment
+
+admin.site.register(Review)
+admin.site.register(Comment)
