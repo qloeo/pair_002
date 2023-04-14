cd6bf69c89a83ebb7a430706d1f3d52250924fe7
seho1278 <seho1278@naver.com>
2023-04-14 15:41:16 +0900

    recomment

--- reviews/models.py
+++ reviews/models.py
@@ -25,3 +25,15 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.content
+    
+
+class ReComment(models.Model):
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content
