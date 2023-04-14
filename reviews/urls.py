e59167c97e0e2f62c2ae79c17680aa5eca564b72
ruby04 <ruby04@SHUREE-MacBook-Air.local>
2023-04-14 10:04:59 +0900

    Update reivews

--- reviews/urls.py
+++ reviews/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('<int:review_pk>', views.detail, name="detail"),
     path('create/', views.create, name="create"),
     path('<int:review_pk>/update', views.update, name="update"),
+    path('<int:review_pk>/delete', views.delete, name="delete"),
+    path('<int:review_pk>/comment', views.create_comment, name="create_comment"),
+    path('<int:review_pk>/comment/<int:comment_pk>/delete', views.delete_comment, name="delete_comment"),
 ]
