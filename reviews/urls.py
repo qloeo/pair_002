c0affb89722ef01a395212ccf64a15d28c201dfc
seho1278 <seho1278@naver.com>
2023-04-14 14:00:22 +0900

    review_comment_like

--- reviews/urls.py
+++ reviews/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path('<int:review_pk>/comment/<int:comment_pk>/delete', views.delete_comment, name="delete_comment"),
     path('<int:review_pk>/prev', views.prev, name="prev"),
     path('<int:review_pk>/next', views.next, name="next"),
+    path('<int:review_pk>/review_like', views.review_like, name="review_like"),
+    path('<int:review_pk>/comment/<int:comment_pk>/comment_like', views.comment_like, name="comment_like"),
 ]
